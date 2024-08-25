f581020906e4baae33290ee893a7e821ac708a6e
abd <abd@abd.abd>
2024-08-25 23:44:27 +0530

    BT

--- config.h
+++ config.h
@@ -92,8 +92,8 @@ static const Key keys[] = {
 	{MODKEY,			XK_F6,	   spawn,		   SHCMD("light -A 5")},
 	{MODKEY,   			XK_F5,     spawn,		   SHCMD("light -U 5")	},
 	{MODKEY,			XK_F2,	   spawn,		   SHCMD("amixer set Master 1%-")},
-	{MODKEY,			XK_F3,	   spawn,		   SHCMD("amixer set Master 1%+")},
-	{MODKEY,			XK_F1,	   spawn,		   SHCMD("amixer set Master toggle")},
+	{0	,			XK_F3,	   spawn,		   SHCMD("amixer set Master 1%+")},
+	{0	,			XK_F1,	   spawn,		   SHCMD("amixer set Master toggle")},
 	{MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
