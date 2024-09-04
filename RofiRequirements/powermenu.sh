b249b84bc6d1c3733f417f4637db5545ad58fd2b
bharani2624 <bharani2624baru@gmail.com>
2024-09-04 20:50:25 +0530

    Added Power Controls Along With Wifi Control(Windows+W)

---
+++ RofiRequirements/powermenu.sh
@@ -0,0 +1,63 @@
+dir="$HOME/.config/rofi/"
+theme='config'
+
+uptime="`uptime -p | sed -e 's/up //g'`"
+host=`hostname`
+
+shutdown=' Shutdown'
+reboot=' Reboot'
+wifi=' WiFi'
+yes=' Yes'
+no=' No'
+
+rofi_cmd() {
+	rofi -dmenu \
+		-p "$host" \
+		-mesg "Uptime: $uptime" \
+		-theme ${dir}/${theme}.rasi
+}
+
+confirm_cmd() {
+	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 150px;}' \
+		-theme-str 'mainbox {children: [ "message", "listview" ];}' \
+		-theme-str 'listview {columns: 2; lines: 1;}' \
+		-theme-str 'element-text {horizontal-align: 0.5;}' \
+		-theme-str 'textbox {horizontal-align: 0.5;}' \
+		-dmenu \
+		-p 'Confirmation' \
+		-mesg 'Just Be OverConfident Like Mr.BenTennyson?' \
+		-theme ${dir}/${theme}.rasi
+}
+
+confirm_exit() {
+	echo -e "$yes\n$no" | confirm_cmd
+}
+
+run_rofi() {
+	echo -e "$shutdown\n$reboot\n$wifi" | rofi_cmd
+}
+
+run_cmd() {
+	selected="$(confirm_exit)"
+	if [[ "$selected" == "$yes" ]]; then
+		if [[ $1 == '--shutdown' ]]; then
+			systemctl poweroff
+		elif [[ $1 == '--reboot' ]]; then
+			systemctl reboot
+		fi
+	else
+		exit 0
+	fi
+}
+chosen="$(run_rofi)"
+case ${chosen} in
+    $shutdown)
+		run_cmd --shutdown
+        ;;
+    $reboot)
+		run_cmd --reboot
+        ;;
+    $wifi) 
+        "$HOME/.config/rofi/wifi.sh"
+        ;;
+esac
---
+++ RofiRequirements/wifi.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+opt="ON/OFF\nConnect"
+selected=$(echo "$opt"|rofi -dmenu -p "WIFI")
+case $selected in
+	"ON/OFF")
+	wifi_status=$(nmcli radio wifi)
+	if [ "$wifi_status" = "enabled" ]; then
+	nmcli radio wifi off
+	dunstify "Successfully Disabled Wifi"
+	else
+	nmcli radio wifi on
+	dunstify "Succedssfully Enabled Wifi"
+	fi
+	;;
+	"Connect")
+	devices=$(nmcli device wifi list|awk 'NR>1 {print $2}')
+	device=$(echo "$devices"|rofi -dmenu -p "Select Wifi To connect")
+	if [ -n "$device" ]; then
+		password=$(rofi -dmenu -password -p "Enter Password For $device")
+		connection=$(nmcli device wifi connect "$device" password "$password"|awk '{print $3}')
+		if [ "$connection" = "successfully" ]; then
+		dunstify "Successfully Connected"
+		else
+		dunstify "Password is incorrect"
+		fi
+	else
+	dunstify "No Network Selected"
+	fi
+	;;
+esac
