493bea09f4d5447fef69161ef03a549338a2602d
bharani2624 <bharani2624baru@gmail.com>
2024-09-05 09:22:38 +0530

    Made Some Changes In Install.sh

--- RofiRequirements/powermenu.sh
+++ RofiRequirements/powermenu.sh
@@ -41,7 +41,7 @@ run_cmd() {
 	selected="$(confirm_exit)"
 	if [[ "$selected" == "$yes" ]]; then
 		if [[ $1 == '--shutdown' ]]; then
-			systemctl poweroff
+			sudo systemctl poweroff
 		elif [[ $1 == '--reboot' ]]; then
 			systemctl reboot
 		fi
