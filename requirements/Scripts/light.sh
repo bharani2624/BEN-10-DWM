d0123d407b44cbc204f739b24ebc981b03ca44e7
bharani2624 <bharani2624baru@gmail.com>
2024-09-06 16:09:42 +0530

    Added BT In powermenu

---
+++ requirements/Scripts/light.sh
@@ -0,0 +1,7 @@
+feh --bg-fill "/home/abd/ben10W/ABD.jpg"
+picom &
+slstatus &
+dunst &
+xinput set-prop "MSFT0002:00 04F3:31AD Touchpad" "libinput Tapping Enabled" 1 &
+xinput set-prop "MSFT0002:00 04F3:31AD Touchpad" "libinput Natural Scrolling Enabled" 1 & 
+exec dwm
