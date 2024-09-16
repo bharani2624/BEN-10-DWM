646215d4b5641be6842bba54287010f37b4813d0
bharani2624 <bharani2624baru@gmail.com>
2024-09-16 15:26:20 +0530

    Made The Install Script Work

--- install.sh
+++ install.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
+echo "Started To Add Configuration Files Kindly Enter 'Y' If Asked"
+
+echo "Started Installing Dependencies"
 sudo apt install build-essential libx11-dev libxft-dev libxinerama-dev libfreetype6-dev libfontconfig1-dev
 sudo apt install libxrandr-dev feh dunst sddm picom alacritty scrot
 curl -sS https://starship.rs/install.sh | sh
-touch ~/.xinitrc
-cat<<EOF > ~/.xinitrc
-feh --bg-fill ~/Downloads/BEN-10-DWM/10.jpg
-picom &
-slstatus &
-dunst &
-xinput set-prop "MSFT0002:00 04F3:31AD Touchpad" "libinput Tapping Enabled" 1 &
-xinput set-prop "MSFT0002:00 04F3:31AD Touchpad" "libinput Natural Scrolling Enabled" 1>
-exec dwm
-EOF
-sudo mkdir /usr/share/fonts/truetype/ben10
+echo "Completed installing Dependencies"
+
+echo "....................................................................................."
+
+cp ~/BEN-10-DWM/.xinitrc ~/.xinitrc
+sudo mkdir -p /usr/share/fonts/truetype/ben10
 sudo cp ~/BEN-10-DWM/GROBOLD.ttf /usr/share/fonts/truetype/ben10
-mkdir ~/.config/picom
+mkdir -p ~/.config/picom
 touch ~/.config/picom/picom.conf
+
 
 cat<<EOF >~/.config/picom/picom.conf
 opacity-rule=[
@@ -29,31 +28,24 @@ active-opacity=0.90;
 frame-opacity=1.0;
 EOF
 
-sudo apt install sddm
-sudo apt install picom
-mkdir ~/.config/rofi/shared
-sudo cp ~/BEN-10-DWM/requirements/wifi.sh ~/.config/rofi/
-sudo cp ~/BEN-10-DWM/requirements/bt.sh ~/.config/rofi/
+
+mkdir -p ~/.config/rofi/shared
+sudo cp ~/BEN-10-DWM/requirements/Scripts/wifi.sh ~/.config/rofi/
+sudo cp ~/BEN-10-DWM/requirements/Scripts/bt.sh ~/.config/rofi/
 touch ~/.config/rofi/shared/colors.rasi
-cat
-<<EOF >~/.config/rofi/shared/colors.rasi
+cat<<EOF >~/.config/rofi/shared/colors.rasi
 @import "~/.config/rofi/colors/ben10"
 EOF
 
-echo "$color">~/.config/rofi/shared/colors.rasi
-
 touch ~/.config/rofi/shared/fonts.rasi
 
-cat<<
-EOF >~/.config/rofi/shared/fonts.rasi
+cat<<EOF >~/.config/rofi/shared/fonts.rasi
 * {
     font: "JetBrains Mono Nerd Font 15";
 }
 EOF
-# echo "$fonts">~/.config/rofi/shared/fonts.rasi
-mkdir ~/.config/rofi/colors
 
-# touch ~/.config/rofi/colors/ben10.rasi
+mkdir -p ~/.config/rofi/colors
 
 sudo cp ~/BEN-10-DWM/RofiRequirements/ben10.rasi ~/.config/rofi/colors/ben10.rasi
 sudo cp ~/BEN-10-DWM/RofiRequirements/config.rasi ~/.config/rofi/config.rasi
